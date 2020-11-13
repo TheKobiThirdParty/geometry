a38f4ccdb7ca47fb162051e27f54adbbe1532d17
Alexis Paques <alexis@thekobi.com>
2020-11-13 15:30:58 +0100

    Remove removed folders

--- tf/setup.py
+++ tf/setup.py
@@ -5,10 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['tf'],
-    package_dir={'': 'src'},
-    requires=['genmsg', 'genpy', 'roslib', 'rospkg', 'geometry_msgs', 'sensor_msgs', 'std_msgs'],
-    scripts=['scripts/groovy_compatibility/tf_remap',
-             'scripts/groovy_compatibility/view_frames']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
